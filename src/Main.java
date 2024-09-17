e7aae376ce2a895d1e9aab472749ee86430cee58
BT_2S14_20 <@email>
2024-09-17 10:42:22 -0400

    Completed Project 1, initial take on requirements

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,39 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        //Preparation for user input
+        Scanner s = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        //Variables bill, tip, and numPpl take initial costs and number of people through user input and convert them to double or integer values to be used to calculate final amounts
+        System.out.print("Enter Your Bill Amount (leave out $): ");
+        String a = s.nextLine();
+        double bill = Double.parseDouble(a);
+
+        System.out.print("Enter Tip Amount (leave out %): ");
+        String b = s.nextLine();
+        int tip = Integer.parseInt(b);
+
+        System.out.print("Number of People: ");
+        String c = s.nextLine();
+        int numPpl = Integer.parseInt(c);
+
+        //Calculates total costs in correct double form
+        double totalTip = bill*((double)tip/100);
+        double totalBill = bill + totalTip;
+
+        //Finds tip and bill amount per person
+        double tipPer = totalTip/numPpl;
+        double billPer = totalBill/numPpl;
+
+        //Prints receipt while rounding values to nearest hundredths place to resemble money values
+        System.out.println("---------------------------------------");
+        System.out.println("Total Tip: $" + String.format("%.2f",totalTip));
+        System.out.println();
+        System.out.println("Total Bill: $" + String.format("%.2f",totalBill));
+        System.out.println();
+        System.out.println("Tip per Person: $" + String.format("%.2f",tipPer));
+        System.out.println();
+        System.out.println("Bill per Person: $" + String.format("%.2f",billPer));
+
         }
     }
-}
